b14353f19df53568468412f6cf7cbd840518f736
Andreas Strasser <a.strasser@student.tugraz.at>
2023-11-26 11:39:33 +0100

    added about page

---
+++ stopwatch/app/src/main/java/com/nfgv/stopwatch/fragment/AboutFragment.kt
@@ -0,0 +1,67 @@
+package com.nfgv.stopwatch.fragment
+
+import android.media.MediaPlayer
+import android.os.Bundle
+import android.text.method.LinkMovementMethod
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nfgv.stopwatch.R
+import com.nfgv.stopwatch.databinding.AboutFragmentBinding
+
+
+class AboutFragment : Fragment() {
+    private var _binding: AboutFragmentBinding? = null
+    private lateinit var mediaPlayer: MediaPlayer
+
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = AboutFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initView()
+        initMediaPlayer()
+        registerListeners()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    private fun initView() {
+        binding.textGithub.movementMethod = LinkMovementMethod.getInstance()
+    }
+
+    private fun initMediaPlayer() {
+        try {
+            mediaPlayer = MediaPlayer.create(requireContext(), R.raw.nananana)
+        } catch (e: Exception) {
+            Log.e("MediaPlayer", "Failed to initialize media player $e")
+        }
+    }
+
+    private fun registerListeners() {
+        binding.logoLayout.setOnClickListener { playNananaSound() }
+    }
+
+    private fun playNananaSound() {
+        try {
+            mediaPlayer.stop()
+            mediaPlayer.prepare()
+            mediaPlayer.start()
+        } catch (e: Exception) {
+            Log.e("MediaPlayer", "Failed to play sound $e")
+        }
+    }
+}
